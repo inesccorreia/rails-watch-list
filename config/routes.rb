3eff4511b2a304a39a6bd8642694c68289a9f58b
Ines Correia <inesccorreia@gmail.com>
2022-11-17 16:43:36 +0000

    Exercise Watch List

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,15 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  get "/lists", to: "lists#index"
-  get "/lists/new", to: "lists#new"
-  get "/lists/:id", to: "lists#show", as: :list
-  post "/lists", to: "lists#create"
+  #  get "/lists", to: "lists#index"
+  #  get "/lists/new", to: "lists#new"
+  #  get "/lists/:id", to: "lists#show", as: :list
+  #  post "/lists", to: "lists#create"
+
+  #  get "lists/:id/bookmarks/new", to: "bookmarks#new", as: :new_list_bookmark
+  #  post "lists/:id/bookmarks", to: "bookmarks#create", as: :list_bookmarks
+
+  resources :lists, only: [:index, :new, :show, :create] do
+    resources :bookmarks, only: [:new, :create]
+  end
 end
